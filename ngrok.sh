56af6a4c9dd3381990dfc8049a5ff088423e2c94
AvailedNpnpcy <142320034+NpnpicyAvailable@users.noreply.github.com>
2023-09-13 01:36:32 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Memasukkan token Ngrok ke dalam variabel
-NGROK_TOKEN="2BfdASj8NgWJ9EUqQjYiXzUQgW1_6EA3BmKK7SuHg7BHWeBqV"
-
-# Menjalankan Ngrok dengan token yang telah disetel
-./ngrok authtoken "$NGROK_TOKEN"
-
-# Mengarahkan ke Gotty
-./ngrok http http://localhost:8080
-
 # Menjalankan Gotty dengan perintah khusus
 ./gotty -w bash
