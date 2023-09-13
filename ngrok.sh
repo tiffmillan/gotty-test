2b771ff4641f7f1a66f1acc37ad9996925987f25
AvailedNpnpcy <142320034+NpnpicyAvailable@users.noreply.github.com>
2023-09-13 09:55:34 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -3,11 +3,7 @@
 # Gitu wir
 export DEBIAN_FRONTEND=noninteractive
 
-# Install npm dengan opsi -y untuk menghindari konfirmasi tambahan
-apt install npm -y
-
 # Menginstal PM2 secara global menggunakan npm
-npm install npm@latest -g
 npm install pm2 -g
 
 # Memulai proses-proses yang didefinisikan dalam berkas konfigurasi pm2.json
