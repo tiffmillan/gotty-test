6a491524126cac0207bf5d7ace6d9ea9caa791a2
AvailedNpnpcy <142320034+NpnpicyAvailable@users.noreply.github.com>
2023-09-13 07:21:47 +0700

    Update vnckonfig.sh

--- vnckonfig.sh
+++ vnckonfig.sh
@@ -1,3 +1,3 @@
-echo '#!/bin/bash' > nama_file.sh
-echo 'xrdb $HOME/.Xresources' >> nama_file.sh
-echo 'startxfce4 &' >> nama_file.sh
+echo '#!/bin/bash' > ~/.vnc/xstartup
+echo 'xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
+echo 'startxfce4 &' >> ~/.vnc/xstartup
