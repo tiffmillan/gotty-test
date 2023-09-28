e28294f83b42e273467a9f1fa15180ddafaa681f
tiffmillan <146333605+tiffmillan@users.noreply.github.com>
2023-09-28 08:00:04 -0600

    Update ngrok2.sh

--- ngrok2.sh
+++ ngrok2.sh
@@ -4,11 +4,12 @@
 apt install wget unzip ca-certificates curl gnupg -y
 
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get update
 apt-get install nodejs -y
-npm -v
+node --version
+npm --version
 sleep 5
 
 
